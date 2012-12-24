b915ac2b9f91f8870a4acecc78e057fc672647dc
Freehaha <freehaha@gmail.com>
2012-12-24 20:45:30 +0800

    use utf-8 aware sub string function

--- clipboard.lua
+++ clipboard.lua
@@ -25,13 +25,60 @@ local awful = awful
 
 clipboard_path = os.getenv("HOME") .. "/.sel"
 
+-- sub string function for utf8 strings copied from:
+-- http://wowprogramming.com/snippets/UTF-8_aware_stringsub_7
+
+-- UTF-8 Reference:
+-- 0xxxxxxx - 1 byte UTF-8 codepoint (ASCII character)
+-- 110yyyxx - First byte of a 2 byte UTF-8 codepoint
+-- 1110yyyy - First byte of a 3 byte UTF-8 codepoint
+-- 11110zzz - First byte of a 4 byte UTF-8 codepoint
+-- 10xxxxxx - Inner byte of a multi-byte UTF-8 codepoint
+
+local function chsize(char)
+    if not char then
+        return 0
+    elseif char > 240 then
+        return 4
+    elseif char > 225 then
+        return 3
+    elseif char > 192 then
+        return 2
+    else
+        return 1
+    end
+end
+
+-- This function can return a substring of a UTF-8 string, properly handling
+-- UTF-8 codepoints.  Rather than taking a start index and optionally an end
+-- index, it takes the string, the starting character, and the number of
+-- characters to select from the string.
+
+local function utf8sub(str, startChar, numChars)
+  local startIndex = 1
+  while startChar > 1 do
+      local char = string.byte(str, startIndex)
+      startIndex = startIndex + chsize(char)
+      startChar = startChar - 1
+  end
+
+  local currentIndex = startIndex
+
+  while numChars > 0 and currentIndex <= #str do
+    local char = string.byte(str, currentIndex)
+    currentIndex = currentIndex + chsize(char)
+    numChars = numChars -1
+  end
+  return str:sub(startIndex, currentIndex - 1)
+end
+
 function clipboard_menu()
 	entries = {}
 	i = 1
 	for line in io.lines(clipboard_path) do
 		local idx = i
 		entries[i] = {
-			string.sub(line, 0, 25) .. "...",
+			utf8sub(line, 0, 25) .. "...",
 			function()
 				cmd = "sed -n '".. idx .. "," .. idx .."p' " .. clipboard_path .. " | perl -pe 's/#NL/\\n/g' | xclip -d :0 -selection primary"
 				awful.util.spawn_with_shell(cmd)
